61a1b84f1be72efe78a0394422b77b38532b5c0f
Enes Dindar <enesdindarr@gmail.com>
2022-12-20 15:07:37 +0300

    ilk commit

---
+++ uygulama/lib/view/anasayfa.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class anaSayfa extends StatefulWidget {
+  const anaSayfa({super.key});
+
+  @override
+  State<anaSayfa> createState() => _anaSayfaState();
+}
+
+class _anaSayfaState extends State<anaSayfa> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
