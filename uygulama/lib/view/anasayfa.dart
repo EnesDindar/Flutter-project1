f2125d5f8b2c905d8cf03de8fa839e516181e948
Enes Dindar <enesdindarr@gmail.com>
2022-12-20 18:29:43 +0300

    second commit

--- uygulama/lib/view/anasayfa.dart
+++ uygulama/lib/view/anasayfa.dart
@@ -10,6 +10,19 @@ class anaSayfa extends StatefulWidget {
 class _anaSayfaState extends State<anaSayfa> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+          automaticallyImplyLeading: true,
+          backgroundColor: const Color.fromARGB(137, 90, 89, 89),
+          leading: Icon(Icons.sports_soccer_outlined,
+              color: Colors.lightGreen[300], shadows: const [Shadow(color: Colors.black, blurRadius: 15.0)], size: 40),
+          title: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 60),
+            child: const Text('FIFA MATCH', style: TextStyle(shadows: [Shadow(color: Colors.black, blurRadius: 15.0)])),
+          )),
+      body: Column(
+        children: [],
+      ),
+    );
   }
 }
